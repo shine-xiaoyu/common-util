5873a2cb88ea16dea097ba2a3e59de519681dc43
shine-xiaoyu <923577587@qq.com>
2020-02-11 17:14:44 +0800

    添加CollectionUtil集合工具类

---
+++ src/main/java/com/xqy/common/utils/CollectionUtil.java
@@ -0,0 +1,14 @@
+package com.xqy.common.utils;
+
+import java.util.Collection;
+
+public class CollectionUtil {
+
+	//判断集合是否为空
+	
+	public static boolean isEmpty(Collection<?> collection) {
+		
+		return collection == null || collection.size()==0;
+	}
+	
+}
